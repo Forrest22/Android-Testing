a0ba6b84560e6b440401011d1b423cdd50fb8da1
Forrest22 <forforr@gmail.com>
2013-09-29 23:11:02 -0400

    Created SplashScreen, edited AndroidManifest

--- Test/src/main/java/com/example/test/SplashScreen.java
+++ Test/src/main/java/com/example/test/SplashScreen.java
@@ -1,10 +1,31 @@
 package com.example.test;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 
 /**
  * Created by Forrest on 8/7/13.
  */
 public class SplashScreen extends Activity {
-    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent openStartingPoint = new Intent("com.example.test.STARTINGPOINT");
+                    startActivity(openStartingPoint);
+                }
+            }
+        };
+        timer.start();
+    }
 }
